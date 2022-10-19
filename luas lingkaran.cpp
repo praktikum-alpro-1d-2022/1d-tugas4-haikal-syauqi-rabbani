20eba4afdc128a07d75279c25a376229423938f2
hkllsr <114643336+hkllsr@users.noreply.github.com>
2022-10-19 13:12:13 +0700

    Add files via upload

---
+++ luas lingkaran.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  double r, L;
+
+  cout << "masukkan r: ";
+  cin >> r;
+  L = 3.14 * r * r;
+  cout << "luas lingkaran = " << L;
+
+  return 0;
+}
